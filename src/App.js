0e75d8e9c35ae8c0ded83ccc3da9b9c6fcb38ffd
Achugs <anna_9400@hotmail.com>
2019-08-05 15:08:14 +0100

    organisation of files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/EveryPageComponents/Header'
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/EveryPageComponents/NavBar'
 import { Router } from "@reach/router"
-import Home from './components/Home';
-import Articles from './components/Articles'
+import Home from './components/ArticleComponents/Home';
+import Articles from './components/ArticleComponents/Articles'
 // import Topics from './components/Topics';
 
 
