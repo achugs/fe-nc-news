448142bd6a7be7f0f8827ab4d7c468c7b7fb2948
Achugs <anna_9400@hotmail.com>
2019-08-11 14:19:05 +0100

    styling almost complete, error and loading page complete, needs more responsiveness adding

--- src/ErrorHandlingDisplay.js
+++ src/ErrorHandlingDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styles from './Errors.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGhost } from "@fortawesome/free-solid-svg-icons";
 
 const ErrorHandlingDisplay = (error) => {
   return (
-    <p>ERROR {error.status} {error.msg}</p>
+    <div>
+      <h3 className={styles.ghost}>
+        <FontAwesomeIcon icon={faGhost} />
+      </h3>
+      <p className={styles.error}>ERROR {error.status} {error.msg}</p>
+    </div>
   );
 }
 
--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRing } from "@fortawesome/free-solid-svg-icons";
+import styles from './Loading.module.css';
 
 const Loading = () => {
   return (
     <div>
-      <h3>Loading...</h3>
+      <h3 className={styles.ring}>
+        <FontAwesomeIcon icon={faRing} />
+      </h3>
+      <p className={styles.loading}>Loading...</p>
     </div>
   );
 };
