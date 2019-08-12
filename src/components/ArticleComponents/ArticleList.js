0e503998c5509e1c64e125da7aa086a17a870e3b
Achugs <anna_9400@hotmail.com>
2019-08-12 15:33:49 +0100

    a11y edits made

--- src/components/ArticleComponents/ArticleList.js
+++ src/components/ArticleComponents/ArticleList.js
@@ -7,18 +7,20 @@ import styles from './ArticleList.module.css';
 const ArticleList = (props) => {
   return (
     < div className={styles.articlesTitle} >
-      {props.articles.map(article => {
-        return (
-          <li key={article.title}   >
-            <Link to={`/articles/${article.article_id}`} className={styles.articleTitleLink}><h4 >{article.title}</h4></Link>
-            <p>Level type: {article.topic}</p>
-            <p>Player: {article.author}</p>
-            <p>Spawned: {new Date(article.created_at).toLocaleString()}</p>
-            <p>Lives: {article.votes}</p>
-            <p>Comments: {article.comment_count}</p>
-          </li>
-        )
-      })}
+      <ul>
+        {props.articles.map(article => {
+          return (
+            <li key={article.title}   >
+              <Link to={`/articles/${article.article_id}`} className={styles.articleTitleLink}><h4 >{article.title}</h4></Link>
+              <p>Level type: {article.topic}</p>
+              <p>Player: {article.author}</p>
+              <p>Spawned: {new Date(article.created_at).toLocaleString()}</p>
+              <p>Lives: {article.votes}</p>
+              <p>Comments: {article.comment_count}</p>
+            </li>
+          )
+        })}
+      </ul>
     </ div>
   );
 };
